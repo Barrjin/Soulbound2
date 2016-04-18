d440d77b2b69d39bc5a13193a9b14f79d4930db9
Liz Earl <liz.agnes.earl@gmail.com>
2016-04-18 12:31:25 -0500

    Added files via upload

---
+++ MovieScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class MovieScript : MonoBehaviour {
+
+    public MovieTexture movie;
+    private AudioSource movieAudio;
+
+	void Start ()
+    {
+        GetComponent<RawImage>().texture = movie as MovieTexture;
+        movieAudio = GetComponent<AudioSource>();
+        movieAudio.clip = movie.audioClip;
+        movie.Play();
+        movieAudio.Play();
+  
+	}
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && movie.isPlaying)
+        {
+
+            movie.Pause();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Space) && !movie.isPlaying)
+        {
+
+            movie.Play();
+        }
+    }
+}
